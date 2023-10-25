6bd5c913b9c403b2a79c2558c556aa0369246ce8
IRSi7 <73004001+IRSi7@users.noreply.github.com>
2023-10-25 10:19:30 +0300

    Переделал Controller на RestController и заменил @RequestParameter на @PathVariable где это возможно

--- learning-center-impl/src/main/java/space/irsi7/controllers/StudentController.java
+++ learning-center-impl/src/main/java/space/irsi7/controllers/StudentController.java
@@ -1,64 +1,54 @@
 package space.irsi7.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import space.irsi7.interfaces.StudentService;
 
-@Controller
+@RestController
 @RequestMapping("/students")
 public class StudentController {
 
-    @Autowired
-    private StudentService studentService;
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @PostMapping("/addStudent")
-    @ResponseBody
-    public String addStudent(@RequestParam String name, int courseId) {
+    public void addStudent(@RequestParam String name, int courseId) {
         studentService.addStudent(name, courseId);
-        return "Success";
     }
 
-    @PostMapping("/dropStudent")
-    @ResponseBody
-    public String dropStudent(@RequestParam int id) {
+    @PostMapping("/dropStudent/{id}")
+    public void dropStudent(@PathVariable int id) {
         studentService.removeStudent(id);
-        return "Success";
     }
 
-    @PostMapping("/rateStudent")
-    @ResponseBody
-    public String rateStudent(@RequestParam int id, int mark) {
+    @PostMapping("/rateStudent/{id}")
+    public void rateStudent(@PathVariable int id, int mark) {
         studentService.rateStudent(id, mark);
-        return "Success";
     }
 
-    @GetMapping("/getEduTimeLeft")
-    @ResponseBody
-    public int getEduTimeLeft(@RequestParam int id) {
+    @GetMapping("/getEduTimeLeft/{id}")
+    public int getEduTimeLeft(@PathVariable int id) {
         return studentService.getEduTimeLeft(id);
     }
 
-    @GetMapping("/getDropChance")
-    @ResponseBody
-    public String getDropChance(@RequestParam int id) {
+    @GetMapping("/getDropChance/{id}")
+    public String getDropChance(@PathVariable int id) {
         return studentService.getDropChance(id);
     }
 
-    @GetMapping("/getReport")
-    @ResponseBody
-    public String getReport(@RequestParam int id) {
+    @GetMapping("/getReport/{id}")
+    public String getReport(@PathVariable int id) {
         return studentService.getReportStudent(id);
     }
 
     @GetMapping("/getAllReport")
-    @ResponseBody
     public String getAllReport(@RequestParam int sort, int order, int filter) {
         return studentService.getAllReport(sort, order, filter).toString();
     }
 
     @GetMapping("/AmIRunning")
-    @ResponseBody
     public String test() {
         return "I am running";
     }
