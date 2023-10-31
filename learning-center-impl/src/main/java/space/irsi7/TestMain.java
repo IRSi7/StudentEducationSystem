dc85f82131a0ad02b3f23bb9ceb4bce3383ab7a6
IRSi7 <73004001+IRSi7@users.noreply.github.com>
2023-10-31 10:44:36 +0300

    Исправил замечания

--- learning-center-impl/src/main/java/space/irsi7/TestMain.java
+++ learning-center-impl/src/main/java/space/irsi7/TestMain.java
@@ -7,9 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import space.irsi7.models.Student;
 
-public class Main {
+public class TestMain {
 
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final Logger logger = LoggerFactory.getLogger(TestMain.class);
     public static void main(String[] args) {
         ApplicationContext context
                 = new AnnotationConfigApplicationContext(
